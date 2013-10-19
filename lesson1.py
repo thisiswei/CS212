842883f67bccfc77e9303fb5ef74dda1476a30f8
thisiswei <99plus2@gmail.com>
2013-10-19 19:19:36 -0400

    quiz: write deal function

--- lesson1.py
+++ lesson1.py
@@ -1,3 +1,19 @@
+# Write a function, deal(numhands, n=5, deck), that
+# deals numhands hands with n cards each.
+import random # this will be a useful library for shuffling
+
+# This builds a deck of 52 cards. If you are unfamiliar
+# with this notation, check out Andy's supplemental video
+# on list comprehensions (you can find the link in the
+# Instructor Comments box below).
+
+mydeck = [r+s for r in '23456789TJQKA' for s in 'SHDC']
+
+def deal(numhands, n=5, deck=mydeck):
+    # Your code here.
+
+
+
 # Write a function, allmax(iterable, key=None), that returns
 # a list of all items equal to the max of the iterable,
 # according to the function specified by key.
